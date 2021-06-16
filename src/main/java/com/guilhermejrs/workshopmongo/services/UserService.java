5122c40e52139cdc1b8327e75e4451c5d271ac56
Guilherme Souza <guilhermeJRS@gmail.com>
2021-06-16 12:13:35 -0300

    Atualização de usuário com PUT

--- src/main/java/com/guilhermejrs/workshopmongo/services/UserService.java
+++ src/main/java/com/guilhermejrs/workshopmongo/services/UserService.java
@@ -37,6 +37,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newUser = findById(obj.getId());
+		updateData(newUser, obj);
+		return repo.save(newUser);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
